2b103cc32fb8176c7e03efad1f6e6a7b65d38773
pmsdrh <mohammadsalehramandi@gmail.com>
2025-12-10 15:43:41 +0330

    feat(projects): add Project custom Hook

---
+++ src/hooks/useProjects.ts
@@ -0,0 +1,62 @@
+import { createProjectApi, getProjectsApi, deleteProjectApi, editProjectApi, getProjectApi } from "@/api/projects";
+import { useEffect, useState } from "react";
+
+export default function useProjects() {
+    const [projects, setProjects] = useState<IProject[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const getProjects = () => {
+        setLoading(true);
+        getProjectsApi()
+            .then(setProjects)
+            .catch(setError)
+            .finally(() => setLoading(false))
+
+    }
+
+    const createProject = (data: IProject) => {
+        setLoading(true);
+        createProjectApi(data)
+            .then(getProjects)
+            .catch(setError)
+            .finally(() => setLoading(false))
+
+    }
+
+    const deleteProject = (data: IProject) => {
+        setLoading(true);
+        deleteProjectApi(data)
+            .then(getProjects)
+            .catch(setError)
+            .finally(() => setLoading(false))
+
+    }
+
+    const editProject = (data: IProject) => {
+        setLoading(true);
+        editProjectApi(data)
+            .then(getProjects)
+            .catch(setError)
+            .finally(() => setLoading(false))
+
+    }
+
+    const getProject = getProjectApi;
+
+    useEffect(() => {
+        getProjects();
+    }, [])
+
+    return {
+        projects,
+        loading,
+        error,
+        setProjects,
+        getProjects,
+        createProject,
+        deleteProject,
+        editProject,
+        getProject
+    }
+}
